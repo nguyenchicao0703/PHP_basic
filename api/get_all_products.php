9e54fae2aeb5db92a7a0b77e4053f1aca6e98c29
nguyenchicao0703 <chicao0703@gmail.com>
2023-08-05 15:38:48 +0700

    api forget_password, web reset_password

--- api/get_all_products.php
+++ api/get_all_products.php
@@ -1,5 +1,5 @@
 <?php
-// http://127.0.0.1:1234/api/get_all_products.php
+// http://127.0.0.1:1234/api/get_all_products.php?keyword=1&sort=1&limit=3&page=2
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
@@ -7,8 +7,40 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once('../database/connection.php');
 try {
+    // đọc keyword từ url
+    $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : "";
+    $sort = isset($_GET['sort']) ? $_GET['sort'] : 0;
+    if (!is_numeric($sort)) {
+        $sort = 0;
+    }
+    if ($sort == 0) {
+        $sort = 'p.id asc';
+    } else if ($sort == 1) {
+        $sort = 'p.id desc';
+    } else if ($sort == 2) {
+        $sort = 'p.price asc';
+    } else if ($sort == 3) {
+        $sort = 'p.price desc';
+    }
+    $limit = isset($_GET['limit']) ? $_GET['limit'] : 3;
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    if (!is_numeric($limit)) {
+        $limit = 3;
+    }
+    if (!is_numeric($page)) {
+        $page = 1;
+    }
+    // tính offset
+    $offset = ($page - 1) * $limit;
     // lấy dữ liệu từ db
-    $query = "SELECT id, name, price, quantity, image, description, categoryId FROM products";
+    $query = "SELECT p.id, p.name, p.price, p.quantity, p.image, 
+            p.description, p.categoryId, c.name as 'categoryName' 
+            FROM products p 
+            INNER JOIN categories c 
+            ON p.`categoryId` = c.id 
+            WHERE p.name like '%$keyword%'
+            ORDER BY $sort 
+            LIMIT $offset, $limit";
     $result = $dbConn->query($query);
     $products = $result->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode(
---
+++ web/reset_password.php
@@ -0,0 +1,89 @@
+<?php
+// http://127.0.0.1:3456/reset_password.php?email=abc@gmail&token=123456
+include_once('../database/connection.php');
+try {
+    if (isset($_POST['submit'])) {
+        // khi nhấn nút submit
+        $email = $_POST['email'];
+        $token = $_POST['token'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+        // kiểm tra email và token có trong db không
+        $query = "SELECT id FROM reset_password
+                    WHERE email = '$email'
+                    AND token = '$token'
+                    AND avaiable = 1
+                    -- token chưa được sử dụng
+                    -- token còn hiệu lục trong 1 giờ
+                    AND createdAt >= DATE_SUB(NOW(), INTERVAl 1 HOUR)";
+        $result = $dbConn->query($query);
+        // nếu đã dùng thì không cho reset password
+        if (!$result) {
+            Header("Location: 404.php");
+        }
+        // cập nhật mật khẩu mới
+        $dbConn->query("UPDATE users SET password = '$password' WHERE email='$email'");
+        // cập nhật token đã được sử dụng
+        $dbConn->query("UPDATE reset_password SET avaiable = 0
+                        WHERE email = '$email'
+                        AND token = '$token'");
+        // chuyển hướng về trang login
+        Header("Location: login.php");
+    } else {
+        // khi người dùng bấm link trong email
+        $email = $_GET['email'];
+        $token = $_GET['token'];
+        // kiểm tra email và token có trong url không
+        if (!isset($email) || !isset($token)) {
+            Header("Location: 404.php");
+        }
+        // kiểm tra email và token có trong db không
+        $query = "SELECT id FROM reset_password
+                    WHERE email = '$email'
+                    AND token = '$token'
+                    AND avaiable = 1
+                    -- token chưa được sử dụng
+                    -- token còn hiệu lục trong 1 giờ
+                    AND createdAt >= DATE_SUB(NOW(), INTERVAl 1 HOUR)";
+        $result = $dbConn->query($query);
+        // nếu đã dùng thì không cho reset password
+        if (!$result) {
+            Header("Location: 404.php");
+        }
+    }
+} catch (Exception $e) {
+    Header("Location: 404.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Đổi mật khẩu</title>
+</head>
+
+<body>
+    <div class="container mt-3">
+        <h2>Đổi mật khẩu</h2>
+        <form action="" method="post">
+            <div class="mb-3 mt-3">
+                <label for="password">Mật khẩu mới</label>
+                <input type="hidden" name="email" value="<?php echo $email; ?>">
+                <input type="hidden" name="token" value="<?php echo $token; ?>">
+                <input type="text" class="form-control" placeholder="Enter password" name="password">
+            </div>
+            <div class="mb-3">
+                <label for="confirm_password">Nhập lại mật khẩu mới</label>
+                <input type="text" class="form-control" placeholder="Enter confirm password" name="confirm_password">
+            </div>
+            <button name="submit" type="submit" class="btn btn-primary">Đổi mật khẩu</button>
+        </form>
+    </div>
+</body>
+
+</html>
